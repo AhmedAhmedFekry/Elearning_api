1a1d9b1b35665eb5fa2ef805c6fba70ff2ef63b2
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-04-26 09:29:20 +0200

    Creating viewsets and routers

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,8 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
-# , CourseSerializer, CourseWithContentsSerializer
+from .serializers import SubjectSerializer, CourseSerializer
+# , CourseWithContentsSerializer
 # from .permissions import IsEnrolled
 
 
@@ -30,3 +30,25 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    # @action(detail=True,
+    #         methods=['post'],
+    #         authentication_classes=[BasicAuthentication],
+    #         permission_classes=[IsAuthenticated])
+    # def enroll(self, request, *args, **kwargs):
+    #     course = self.get_object()
+    #     course.students.add(request.user)
+    #     return Response({'enrolled': True})
+
+    # @action(detail=True,
+    #         methods=['get'],
+    #         serializer_class=CourseWithContentsSerializer,
+    #         authentication_classes=[BasicAuthentication],
+    #         permission_classes=[IsAuthenticated, IsEnrolled])
+    # def contents(self, request, *args, **kwargs):
+    #     return self.retrieve(request, *args, **kwargs)
